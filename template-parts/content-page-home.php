4d7840e88f9b4b416d3a68c012420cd001d0aece
papercranecode <papercranecode@users.noreply.github.com>
2019-11-12 16:02:55 +0000

    created content-page-home, added some text in the PHP file under the header

--- template-parts/content-page-home.php
+++ template-parts/content-page-home.php
@@ -11,7 +11,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<div>This is the about page - a comment from the PHP page.</div>
+		<div>This is the home page - a comment from the PHP page.</div>
 	</header><!-- .entry-header -->
 
 	<?php twentysixteen_post_thumbnail(); ?>
